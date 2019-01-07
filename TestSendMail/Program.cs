f76f506cc7bace61fdd87a1fb8b7e1c147aaff34
Kireev, Dmitriy <dmitriy.kireev@atos.net>
2019-01-07 22:28:37 +0300

    Init commit

--- TestSendMail/Program.cs
+++ TestSendMail/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Mime;
 
 namespace TestSendMail
 {
@@ -10,6 +7,43 @@ namespace TestSendMail
     {
         static void Main(string[] args)
         {
+            string SMTPUser = "ivanov.ava2000@yandex.ru";
+            string SMTPPassword = "FUCK";
+            int SmtpPort = 587;
+            string SmtpServer = "smtp.yandex.com";
+
+            System.Net.Mail.MailAddress sender = new System.Net.Mail.MailAddress("ivanov.ava2000@yandex.ru", "", System.Text.Encoding.UTF8);
+
+            System.Net.Mail.MailAddress recipient = new System.Net.Mail.MailAddress("mitran@mail.ru", "", System.Text.Encoding.UTF8);
+
+            System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage(sender, recipient);
+
+            email.BodyEncoding = System.Text.Encoding.UTF8;
+            email.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            System.Net.Mail.AlternateView plainView = System.Net.Mail.AlternateView.CreateAlternateViewFromString(System.Text.RegularExpressions.Regex.Replace("My plan body", @"<(.|\n)*?>", string.Empty), null, MediaTypeNames.Text.Plain);
+
+            System.Net.Mail.AlternateView htmlView = System.Net.Mail.AlternateView.CreateAlternateViewFromString("My HTML body", null, MediaTypeNames.Text.Html);
+
+            email.AlternateViews.Clear();
+            email.AlternateViews.Add(plainView);
+            email.AlternateViews.Add(htmlView);
+            email.Subject = $"Azure {DateTime.Now}";
+
+            System.Net.Mail.SmtpClient SMTP = new System.Net.Mail.SmtpClient();
+            SMTP.UseDefaultCredentials = false;
+
+            SMTP.Host = SmtpServer;
+            SMTP.Port = SmtpPort;
+
+            SMTP.Credentials = new System.Net.NetworkCredential(SMTPUser, SMTPPassword);
+            SMTP.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+
+            SMTP.EnableSsl = true;
+
+            SMTP.Send(email);
         }
     }
 }
+
+
